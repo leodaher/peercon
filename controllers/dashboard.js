49e4f65d33a9167dddbfd9d04666f102d9fcb87e
leodaher <leonardo.daher@usp.br>
2016-11-29 01:01:25 -0200

    Update investor form

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -39,6 +39,10 @@ router.post("/cadastro-investidor", multipartMiddleware, middleware.isLoggedIn, 
 	var city = req.body.city;
 	var state = req.body.state;
 
+	var rg = "tmp/"+"investidores/"+req.files.rg.name;
+	var rgverso = "tmp/"+"investidores/"+req.files.rgverso.name;
+	var residencia = "tmp/"+"investidores/"+req.files.residencia.name;
+
 	var images = [req.files.rg,req.files.rgverso,req.files.residencia];
 
 
@@ -143,7 +147,114 @@ router.get("/cadastro-investidor/editar", middleware.isLoggedIn, function(req, r
 			res.render("editCadastroInvestidor",{investor: investor});
 		}
 	});
-})
+});
+
+router.post("/cadastro-investidor/editar", multipartMiddleware, middleware.isLoggedIn, function(req, res){
+	var name = req.body.name;
+	var CPF = req.body.cpf;
+	var birthday = req.body.date;
+	var phone = req.body.telephone;
+	var cellphone = req.body.cellphone;
+	var renda = req.body.renda;
+
+	var cep = req.body.cep;
+	var logradouro = req.body.logradouro;
+	var number = req.body.number;
+	var complement = req.body.complement;
+	var city = req.body.city;
+	var state = req.body.state;
+
+
+	var rg = "tmp/"+"investidores/"+req.files.rg.name;
+	var rgverso = "tmp/"+"investidores/"+req.files.rgverso.name;
+	var residencia = "tmp/"+"investidores/"+req.files.residencia.name;
+
+	var images = [req.files.rg,req.files.rgverso,req.files.residencia];
+
+
+	req.checkBody('name','Nome é um campo obrigatório!').notEmpty();
+	req.checkBody('cpf','CPF é um campo obrigatório!').notEmpty();
+	req.checkBody('date','Data de nascimento é um campo obrigatório!').notEmpty();
+	req.checkBody('telephone','Telefone fixo é um campo obrigatório!').notEmpty();
+	req.checkBody('cellphone','Celular é um campo obrigatório!').notEmpty();
+	req.checkBody('renda','Renda Mensal Declarada é um campo obrigatório!').notEmpty();
+
+	req.checkBody('cep','CEP é um campo obrigatório!').notEmpty();
+	req.checkBody('logradouro','Logradouro é um campo obrigatório!').notEmpty();
+	req.checkBody('number','Número é um campo obrigatório!').notEmpty();
+	req.checkBody('city','Cidade é um campo obrigatório!').notEmpty();
+	req.checkBody('state','Estado é um campo obrigatório!').notEmpty();
+
+	var errors = new Array();
+	if(req.validationErrors()) {
+		errors = req.validationErrors();
+	}
+	
+	// Validar CPF
+	var cpfstr = CPF.replace(/[^0-9]/g,"");
+	console.log(cpfstr);
+	if(!(formValidator.testaCPF(cpfstr))){
+		errors.push({
+			msg: "CPF inválido!"
+		});
+	} 
+
+	if(errors.length > 0) {
+		res.render("cadastroInvestidor",{
+			errors: errors
+		});
+	} else {
+		var newInvestor = {
+			name: name,
+			CPF: CPF,
+			birthday: birthday,
+			phone: phone,
+			cellphone: cellphone,
+			renda: renda,
+			cep: cep,
+			logradouro: logradouro,
+			numero: number,
+			complemento: complement,
+			cidade: city,
+			estado: state,
+		};
+
+		if(images[0].size > 0) {
+			newInvestor.rg = rg;
+		}
+
+		if(images[1].size > 0) {
+			newInvestor.rgverso = rgverso;
+		}
+
+		if(images[2].size > 0) {
+			newInvestor.residencia = residencia;
+		}
+
+		var query = {user: req.user._id};
+
+		Investor.findOneAndUpdate(query, { $set: newInvestor}, function(err){
+			if(err) {
+				console.log(err);
+			} else {
+				images.forEach(function(img){
+					if(img.size > 0) {
+						fs.readFile(img.path, function(err, data){
+							var imageName = img.name;
+							fs.writeFile("tmp/investidores/"+req.user._id+"/"+imageName, data, function(err){
+								if(err) {
+									console.log(err);
+								} else {
+									console.log("Images Saved!");
+								}
+							});
+						});
+					}
+				});
+			}
+		})
+	}
+});
 
 router.get("/cadastro-empresa", middleware.isLoggedIn, function(req, res){
 	res.render("cadastroEmpresa");
